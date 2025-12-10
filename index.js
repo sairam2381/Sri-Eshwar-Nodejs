f65aab03a032c17167a01260e1076dec0f464a08
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-10 10:28:21 +0530

    Db connect

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const database = require("./db-connect/db");
 let port = 21000;
 const app = express();
+database.connect();
+app.use(cors());
+app.use(express.json());
 
-app.use(
-  cors({
-    origin: "*",
-    credientials: true,
-  })
-);
 app.get("/", (req, res) => {
-  return {
+  res.status(200).json({
     success: true,
     message: "Port is running",
-  };
+  });
 });
-app.listen(port, async () => {
+
+app.listen(port, () => {
   console.log(`App is running on the port ${port}`);
 });
