6cedd2f79dfeefe9b3eff37207355033f40ce164
Sreesairam V <sairam2381@users.noreply.github.com>
2025-12-10 14:40:10 +0530

    Pushing the details

--- apis-function/user-functions.js
+++ apis-function/user-functions.js
@@ -1,11 +1,28 @@
 const User = require("../models/user");
 exports.createUser = async (req, res) => {
   try {
+    const { details } = req.body;
     const creatUser = await User.insertOne({
-      firstName: "krish",
-      lastName: "Eshwar",
-      email: "random@gmail.com",
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
     });
+    return res.status(200).json({
+      success: true,
+      message: "User is created successfully",
+    });
+  } catch (e) {
+    res.status(404).json({
+      success: false,
+      error: e,
+    });
+  }
+};
+
+exports.createManyUsers = async (req, res) => {
+  try {
+    const { details } = req.body;
+    const createUser = await User.insertMany(details);
     return res.status(200).json({
       success: true,
       message: "User is created successfully",
--- routes/basic-routes.js
+++ routes/basic-routes.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const { createUser } = require("../apis-function/user-functions");
+const {
+  createUser,
+  createManyUsers,
+} = require("../apis-function/user-functions");
 const router = express.Router();
 router.post("/userSignup", createUser);
-
+router.post("/userSignupMany", createManyUsers);
 module.exports = router;
